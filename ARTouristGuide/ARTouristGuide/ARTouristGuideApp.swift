300e641d57a3137917fbb787f72d3bdc8e5f2142
Tarciziu <tarciziu99christin@yahoo.com>
2022-09-20 15:18:45 +0300

    Initial commit

---
+++ ARTouristGuide/ARTouristGuide/ARTouristGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARTouristGuideApp.swift
+//  ARTouristGuide
+//
+//  Created by Tarciziu Gologan on 20.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ARTouristGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
